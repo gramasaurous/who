b374ce6a933ff63af631c91555bab5f249a35c9b
Graham Greving <grahamgreving@gmail.com>
2016-09-02 22:21:23 -0700

    init() of Who and Who.Light completed. basic tests

--- test.py
+++ test.py
@@ -1,6 +1,8 @@
 #!/usr/binpython
 import sys
+import requests
 import who
+
 
 # Simple test cases to outline how the interface should work
 # and to test the individual methods
@@ -9,34 +11,38 @@ def test_harness(bridge_ip, api_token):
 	# first arg is the IP of the hue bridge
 	# second arg is the api token generated (in the future, if this is blank,
 	# the interface can generate one)
-	mywho = Who(bridge_ip, api_token)
+	try:
+		mywho = who.Who(bridge_ip, api_token)
+		print(mywho)
+	except requests.exceptions.Timeout:
+		print("probably bad connection to the bridge")
 
-	# Toggle all of the lights belonging to mywho
-	for light in mywho.lights:
-		light.toggle()
-	# or alternatively:
-	mywho.all_toggle()
-	# on
-	mywho.all_on()
-	# off
-	mywho.all_off()
+	# # Toggle all of the lights belonging to mywho
+	# for light in mywho.lights:
+	# 	light.toggle()
+	# # or alternatively:
+	# mywho.all_toggle()
+	# # on
+	# mywho.all_on()
+	# # off
+	# mywho.all_off()
 
-	# Toggle a single light by name
-	mywho.toggle("bob")
+	# # Toggle a single light by name
+	# mywho.toggle("bob")
 
-	# Toggle a single light by id
-	mywho.toggle(1)
+	# # Toggle a single light by id
+	# mywho.toggle(1)
 
-	# Turn on/off a single light
-	mywho.on("bob")
-	mywho.on(1)
-	mywho.off("bob")
-	mywho.off(1)
+	# # Turn on/off a single light
+	# mywho.on("bob")
+	# mywho.on(1)
+	# mywho.off("bob")
+	# mywho.off(1)
 
-	# force an update of the bridge state
-	mywho.update() 
-	# force an update of all the lights state
-	mywho.all_update()
+	# # force an update of the bridge state
+	# mywho.update() 
+	# # force an update of all the lights state
+	# mywho.all_update()
 
 if __name__ == '__main__':
 	if len(sys.argv) != 3:
--- who.py
+++ who.py
@@ -5,6 +5,60 @@
 # 09/02/2016
 # Simple Python interface for Hue lights
 
-# who class defines a connection to a specific hue device and it's lights
+import logging
+import requests
+
+# who class defines a connection to a specific hue device and its lights
 class Who:
-	
+	def __repr__(self):
+		string = self.ip
+		for l in self.lights:
+			string += "\t" + str(l) + "\n"
+		return string
+
+	def __init__(self, bridge_ip, api_token, logger=None):
+		self.logger = logger or logging.getLogger(__name__)
+		self.ip = bridge_ip
+		self.api_token = api_token
+		self.lights = []
+		# get_lights() is where connection to bridge is attempted
+		# and will throw an appropriate exception if something goes wrong
+		# see get_lights() for documentation
+		self.get_lights()
+
+	def get_lights(self):
+		try:
+			url = "http://" + self.ip + "/api/" + self.api_token + "/lights"
+			r = requests.get(url, timeout=2)
+			r.raise_for_status()
+			lights = r.json()
+			for light in lights:
+				self.lights.append(self.Light(self, light, lights[light]))
+		except requests.exceptions.HTTPError:
+			self.logger.error("Get Lights Request recieved error code: " + r.status_code)
+			raise
+		except requests.exceptions.Timeout:
+			self.logger.exception("Connection to bridge timed out")
+			raise
+		except Exception:
+			self.logger.exception("Get Lights Request Caught Exception")
+			raise
+
+	class Light:
+		def __repr__(self):
+			string = "light #" + self.id + " (" + self.name + ")"
+			return string
+
+		def __init__(self, bridge, id, attrs):
+			try:
+				self.bridge = bridge
+				self.id = id
+				self.name = attrs['name']
+				self.swversion = attrs['swversion']
+				self.manufacturename = attrs['manufacturername']
+				self.state = attrs['state']
+				self.uniqueid = attrs['uniqueid']
+				self.modelid = attrs['modelid']
+			except Exception:
+				self.bridge.logger.error("Light init caught exception. probably bad json deref")
+				raise
